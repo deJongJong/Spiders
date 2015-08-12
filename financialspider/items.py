9f34ed947d39471c193341aa92c6f580aaa54060
Rolf de Jong <rolfdejong@outlook.com>
2015-08-13 00:26:07 +0200

    Renamed a spider.

--- financialspider/items.py
+++
@@ -1,6 +0,0 @@
-from scrapy.item import Item, Field
-
-class Article(Item):
-	title   = Field()
-	lead    = Field()
-	pubDate = Field()
--- financialspider/settings.py
+++
@@ -1,8 +0,0 @@
-SPIDER_MODULES = ['financialspider.spiders']
-NEWSPIDER_MODULE = 'financialspider.spiders'
-DEFAULT_ITEM_CLASS = 'financialspider.items.Article'
-ITEM_PIPELINES = {
-	'financialspider.pipelines.ArticlePipeline': 300,
-}
-FEED_FORMAT = 'csv'
-FEED_URI = 'file:///tmp/export.csv'
